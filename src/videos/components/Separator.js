286d420927fe67923580ddb1bae99d7444679d19
Daniel H <daguiheso@gmail.com>
2020-02-28 15:05:42 -0500

    create Separator component

---
+++ src/videos/components/Separator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Separator = props => {
+  const {color} = props;
+  const defineBorder = () => {
+    return {borderTopColor: color ? color : '#eaeaea'};
+  };
+
+  return (
+    <View style={[styles.separator, defineBorder()]}>
+      <Text>...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    borderTopWidth: 1,
+  },
+});
+
+export default Separator;
