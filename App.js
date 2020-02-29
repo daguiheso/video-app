a2c8c71b0162e2960524b8fb443cf3830a29f67e
Daniel H <daguiheso@gmail.com>
2020-02-28 22:31:00 -0500

    implemented getMovies method

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
+import API from './utils/api';
 
 const App: () => React$Node = () => {
+  const [suggestionList, setSuggestionList] = useState([]);
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = async () => {
+    const movies = await API.getSeggestion(10);
+    setSuggestionList(movies);
+  };
+
   return (
     <Home>
       <Header />
       <Text>Buscador</Text>
       <Text>Categorias</Text>
       <Text>Sugerencias</Text>
-      <SuggestionList />
+      <SuggestionList list={suggestionList} />
     </Home>
   );
 };
