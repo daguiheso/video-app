65e4faf8ebb78256733faaf5db303a29e5183fd8
Daniel H <daguiheso@gmail.com>
2020-02-29 21:28:35 -0500

    implemented CategoryList component

--- App.js
+++ App.js
@@ -3,17 +3,25 @@ import {Text} from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
+import CategoryList from './src/videos/containers/CategoryList';
 import API from './utils/api';
 
 const App: () => React$Node = () => {
   const [suggestionList, setSuggestionList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
   useEffect(() => {
     getMovies();
+    getCategories();
   }, []);
 
   const getMovies = async () => {
     const movies = await API.getSeggestion(10);
     setSuggestionList(movies);
+  };
+
+  const getCategories = async () => {
+    const movies = await API.getMovies();
+    setCategoryList(movies);
   };
 
   return (
@@ -22,6 +30,7 @@ const App: () => React$Node = () => {
       <Text>Buscador</Text>
       <Text>Categorias</Text>
       <Text>Sugerencias</Text>
+      <CategoryList list={categoryList} />
       <SuggestionList list={suggestionList} />
     </Home>
   );
