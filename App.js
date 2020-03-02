203757927f5c54ac96b32e89edb6d876c59bef0d
Daniel H <daguiheso@gmail.com>
2020-03-01 19:23:19 -0500

    test simple video

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import CategoryList from './src/videos/containers/CategoryList';
 import API from './utils/api';
+import Video from 'react-native-video';
 
 const App: () => React$Node = () => {
   const [suggestionList, setSuggestionList] = useState([]);
@@ -27,6 +28,13 @@ const App: () => React$Node = () => {
   return (
     <Home>
       <Header />
+      <View style={{flex: 1, height: 200,}}>
+        <Video
+          source={{uri:'http://techslides.com/demos/sample-videos/small.mp4'}}
+          style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+          resizeMode="contain"
+        />
+      </View>
       <Text>Buscador</Text>
       <Text>Categorias</Text>
       <Text>Sugerencias</Text>
