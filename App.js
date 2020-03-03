5fbdf6c89ea2820a513b63b6d615af41b13595cf
Daniel H <daguiheso@gmail.com>
2020-03-03 10:57:31 -0500

    create basic redux store

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
+import {Provider} from 'react-redux';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import CategoryList from './src/videos/containers/CategoryList';
 import Player from './src/player/containers/Player';
 import API from './utils/api';
+import store from './store';
 
 const App: () => React$Node = () => {
   const [suggestionList, setSuggestionList] = useState([]);
@@ -26,15 +28,17 @@ const App: () => React$Node = () => {
   };
 
   return (
-    <Home>
-      <Header />
-      <Player />
-      <Text>Buscador</Text>
-      <Text>Categorias</Text>
-      <Text>Sugerencias</Text>
-      <CategoryList list={categoryList} />
-      <SuggestionList list={suggestionList} />
-    </Home>
+    <Provider store={store}>
+      <Home>
+        <Header />
+        <Player />
+        <Text>Buscador</Text>
+        <Text>Categorias</Text>
+        <Text>Sugerencias</Text>
+        <CategoryList list={categoryList} />
+        <SuggestionList list={suggestionList} />
+      </Home>
+    </Provider>
   );
 };
 
