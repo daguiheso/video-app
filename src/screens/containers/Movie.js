2238285494a193e2d3331aee451cbe3763847275
Daniel H <daguiheso@gmail.com>
2020-03-06 21:42:34 -0500

    implemented Close component

--- src/screens/containers/Movie.js
+++ src/screens/containers/Movie.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import MovieLayout from '../components/MovieLayout';
 import Player from '../../player/containers/Player';
 import Header from '../../sections/components/Header';
+import Close from '../../sections/components/Close';
 
-const Movie = () => {
+const Movie = props => {
+  const closeVideo = () => {
+    props.dispatch({
+      type: 'SET_SELECTED_MOVIE',
+      payload: {
+        movie: null,
+      },
+    });
+  };
+
   return (
     <MovieLayout>
-      <Header />
+      <Header>
+        <Close onPress={closeVideo} />
+      </Header>
       <Player />
     </MovieLayout>
   );
 };
 
-export default Movie;
+export default connect(null)(Movie);
