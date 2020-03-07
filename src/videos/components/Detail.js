8b0e26beeeebaedd2e0e62092dff1cbc85bc6592
Daniel H <daguiheso@gmail.com>
2020-03-07 01:01:25 -0500

    implemented WebView for iframe video and ScrollView on Detail component

--- src/videos/components/Detail.js
+++ src/videos/components/Detail.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const Detail = ({title, description_full, medium_cover_image}) => {
+const Detail = ({
+  title,
+  description_full,
+  medium_cover_image,
+  yt_trailer_code,
+}) => {
+  const makeHTML = id => {
+    return `
+      <style>
+        .video {
+          position: relative;
+          padding-bottom: 56.25%;
+        }
+        iframe {
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      </style>
+      <div class="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/${id}"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+      </div>`;
+  };
+
   return (
-    <View>
+    <ScrollView>
       <View style={styles.top}>
         <Text>{title}</Text>
       </View>
@@ -13,14 +48,18 @@ const Detail = ({title, description_full, medium_cover_image}) => {
           <Text style={styles.description}>{description_full}</Text>
         </View>
       </View>
-    </View>
+      <View style={styles.trailer}>
+        <WebView source={{html: makeHTML(yt_trailer_code)}} />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
   trailer: {
-    height: 200,
+    height: 230,
+    marginBottom: 10,
   },
   details: {
     flexDirection: 'row',
