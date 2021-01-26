0b463f47a92a5f12430c690d8e2d8e2c9919d691
Daniel H <daguiheso@gmail.com>
2021-01-26 12:46:27 -0500

    last commit

--- App.js
+++ App.js
@@ -5,12 +5,21 @@ import {PersistGate} from 'redux-persist/integration/react';
 import Loading from './src/sections/components/Loading';
 import {store, persistor} from './store';
 import AppLayout from './src/App';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Text} from 'react-native';
+
+const StackParent = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={<Loading />} persistor={persistor}>
-        <AppLayout />
+        <NavigationContainer>
+          <StackParent.Navigato>
+            <StackParent.Screen name="Home" component={<Text>Pepe</Text>} />
+          </StackParent.Navigato>
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
